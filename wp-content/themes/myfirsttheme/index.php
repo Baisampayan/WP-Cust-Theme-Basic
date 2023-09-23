3a265704e7853d3536ca5e582c42b201964525ff
Baisampayan <baisampayandas@gmail.com>
2023-09-23 20:55:00 +0530

    Adding and initializing footer and header files

--- wp-content/themes/myfirsttheme/index.php
+++ wp-content/themes/myfirsttheme/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First Custom Theme</title>
-</head>
-<body>
+<?php get_header( ); ?>
     
-</body>
-</html>
+<?php get_footer( ); ?>
