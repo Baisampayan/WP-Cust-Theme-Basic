92dc4e61d43229d32140e1901ecf398bf8b48d7d
Baisampayan <baisampayandas@gmail.com>
2023-09-27 22:15:09 +0530

    Structuring the Post Loop, like date, author, etc

--- wp-content/themes/myfirsttheme/index.php
+++ wp-content/themes/myfirsttheme/index.php
@@ -3,6 +3,13 @@
         while(have_posts( )){
             the_post( ); { ?>
                 <a href="<?php the_permalink( ) ?>" title="<?php the_title_attribute( ); ?>"><h2><?php the_title( ); ?></h2></a>
+                <div>
+                    Posted on
+                    <a href="<?php echo get_the_permalink( ); ?>">
+                        <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+                    </a>
+                     | By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php echo get_the_author( ); ?></a>
+                </div>
                 <?php the_content( ); ?>
             <?php }
         }
