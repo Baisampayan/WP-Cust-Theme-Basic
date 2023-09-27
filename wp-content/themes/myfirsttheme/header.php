a9da94edd2f70fc67fcaa4b3ca109198c86d8a28
Baisampayan <baisampayandas@gmail.com>
2023-09-27 11:41:41 +0530

    Setting wp_head for dynamic title, scripts, styles to show in the frontend

--- wp-content/themes/myfirsttheme/header.php
+++ wp-content/themes/myfirsttheme/header.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First Custom Theme</title>
+    <?php wp_head( ); ?>
 </head>
 <body>
