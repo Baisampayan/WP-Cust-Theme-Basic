7fe2c8c0b7fe46d6ff671b8934b85d04545d4082
Baisampayan <baisampayandas@gmail.com>
2023-09-27 21:59:14 +0530

    Adding the Loop - For the posts

--- wp-content/themes/myfirsttheme/index.php
+++ wp-content/themes/myfirsttheme/index.php
@@ -1,3 +1,12 @@
 <?php get_header( ); ?>
-    
+    <?php if(have_posts( )) {
+        while(have_posts( )){
+            the_post( ); { ?>
+                <a href="<?php the_permalink( ) ?>" title="<?php the_title_attribute( ); ?>"><h2><?php the_title( ); ?></h2></a>
+                <?php the_content( ); ?>
+            <?php }
+        }
+    } else { ?>
+        <p>There is no post available according to your criteria.</p>
+    <?php } ?>
 <?php get_footer( ); ?>
