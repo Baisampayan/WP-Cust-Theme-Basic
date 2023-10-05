4be83e7f7b80c2d1d03a2bed6078319a625eb4a3
Baisampayan <baisampayandas@gmail.com>
2023-10-05 10:40:54 +0530

    Setting Translation in the helper.php

--- wp-content/themes/myfirsttheme/lib/helpers.php
+++ wp-content/themes/myfirsttheme/lib/helpers.php
@@ -1,11 +1,13 @@
 <?php
 
 function myfirsttheme_post_meta_template() {
-    echo 'Posted on ';
-    echo '<a href="' . esc_url(get_the_permalink( )) . '">';
-        echo '<time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time>';
-    echo '</a>';
-    echo '&nbsp| By <a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . '</a>';
+    printf(esc_html__( 'Posted on %s', 'myfirsttheme' ),
+        '<a href="' . esc_url(get_the_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time></a>'
+    );
+
+    printf(esc_html__( ' | By %s', 'myfirsttheme' ),
+        '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . '</a>'
+    );
 }
 
 function myfirsttheme_readmore_template() {
