513a5a5fd9a990299a44d80c48ea789ecc6c475a
Baisampayan <baisampayandas@gmail.com>
2023-10-15 01:08:22 +0530

    Setting wp_kses in helpers.php

--- wp-content/themes/myfirsttheme/lib/helpers.php
+++ wp-content/themes/myfirsttheme/lib/helpers.php
@@ -1,16 +1,31 @@
 <?php
 
 function myfirsttheme_post_meta_template() {
+    // translators: %s: Post Date
     printf(esc_html__( 'Posted on %s', 'myfirsttheme' ),
         '<a href="' . esc_url(get_the_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) . '">' . esc_html(get_the_date()) . '</time></a>'
     );
 
+    // translators: %s: Post Author
     printf(esc_html__( ' | By %s', 'myfirsttheme' ),
         '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ) )) . '">' . esc_html(get_the_author( )) . '</a>'
     );
 }
 
 function myfirsttheme_readmore_template() {
-    echo '<a href="' . esc_url(get_permalink( )) . '" title="' . the_title_attribute(['echo'=> false]) . '"> Read More<span class="u-screen-reader-text"> About ' . get_the_title( ) . '</span></a>';
+    echo '<a href="' . esc_url(get_permalink( )) . '" title="' . the_title_attribute(['echo'=> false]) . '">';
+    // translators: %s: Post Title
+    printf(
+        wp_kses(
+        __( 'Read More<span class="u-screen-reader-text"> About %s</span>', 'myfirsttheme' ), 
+        [
+            'span' => [
+                'class' => []
+            ]
+        ]
+        ),
+        get_the_title( )
+    );
+    echo '</a>';
 }
 ?>
