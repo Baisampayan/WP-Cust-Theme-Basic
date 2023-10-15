d1d4ba9b6e985b972afa725229308764ebc5e892
Baisampayan <baisampayandas@gmail.com>
2023-10-16 00:03:03 +0530

    Bug Fixes for admin.css

--- wp-content/themes/myfirsttheme/lib/enqueue-assets.php
+++ wp-content/themes/myfirsttheme/lib/enqueue-assets.php
@@ -4,10 +4,10 @@ function myfirsttheme_assets() {
     wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
 }
 
-add_action( 'wp_enqueue_style', 'myfirsttheme_assets' );
+add_action( 'wp_enqueue_scripts', 'myfirsttheme_assets' );
 
 function myfirsttheme_admin_assets() {
-    wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/admin.css', array(), '1.0.0', 'all' );
 }
 
-add_action( 'admin_enqueue_style', 'myfirsttheme_assets' );
+add_action( 'admin_enqueue_scripts', 'myfirsttheme_admin_assets' );
