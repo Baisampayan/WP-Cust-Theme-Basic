af233c57e8302db3397ca7c44e10966f7c091efa
Baisampayan <baisampayandas@gmail.com>
2023-10-15 23:40:25 +0530

    Adding admin.css and creating multiple css files for dist folder

--- wp-content/themes/myfirsttheme/lib/enqueue-assets.php
+++ wp-content/themes/myfirsttheme/lib/enqueue-assets.php
@@ -10,4 +10,4 @@ function myfirsttheme_admin_assets() {
     wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
 }
 
-add_action( 'wp_enqueue_style', 'myfirsttheme_admin_assets' );
+add_action( 'admin_enqueue_style', 'myfirsttheme_assets' );
