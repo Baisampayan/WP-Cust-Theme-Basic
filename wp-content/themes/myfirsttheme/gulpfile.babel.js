7bee4af1e1fc4c0a918c74e522456404e2f24a8c
Baisampayan <baisampayandas@gmail.com>
2023-10-15 15:20:04 +0530

    Installing and testing gulp-scss

--- wp-content/themes/myfirsttheme/gulpfile.babel.js
+++ wp-content/themes/myfirsttheme/gulpfile.babel.js
@@ -1,11 +1,13 @@
-import { Gulp } from "gulp";
+import gulp from "gulp";
 import yargs from "yargs";
+const sass = require('gulp-sass')(require('sass'));
 
 const PRODUCTION = yargs.argv.prod;
 
-export const hello = (cb) => {
-    console.log(PRODUCTION);
-    cb();
+export const styles = () => {
+    return gulp.src('src/assets/scss/bundle.scss')
+        .pipe(sass().on('ScssError', sass.logError))
+        .pipe(gulp.dest('dist/assets/css'));
 }
 
-export default hello;
+// export default hello;
