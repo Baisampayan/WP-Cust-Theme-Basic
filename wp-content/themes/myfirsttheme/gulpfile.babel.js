d756ca72efc88d5afda6c375a16be1c9425409a9
Baisampayan <baisampayandas@gmail.com>
2023-10-15 20:11:21 +0530

    Installing CSSNano for CSS optimization - npm install gulp-cssnano --save-dev

--- wp-content/themes/myfirsttheme/gulpfile.babel.js
+++ wp-content/themes/myfirsttheme/gulpfile.babel.js
@@ -1,12 +1,16 @@
 import gulp from "gulp";
 import yargs from "yargs";
 const sass = require('gulp-sass')(require('sass'));
+import cssnano from "gulp-cssnano";
+//const clean = require('gulp-clean');
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
     return gulp.src('src/assets/scss/bundle.scss')
         .pipe(sass().on('ScssError', sass.logError))
+        //.pipe(clean())
+        .pipe(cssnano())
         .pipe(gulp.dest('dist/assets/css'));
 }
 
