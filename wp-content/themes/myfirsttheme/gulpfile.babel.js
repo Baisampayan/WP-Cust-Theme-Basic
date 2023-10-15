327f7b9a6547b73c14aeb2a6618585e4cce78e03
Baisampayan <baisampayandas@gmail.com>
2023-10-15 20:41:02 +0530

    Installing GulpIf: npm install gulp-if --save-dev

--- wp-content/themes/myfirsttheme/gulpfile.babel.js
+++ wp-content/themes/myfirsttheme/gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import yargs from "yargs";
 const sass = require('gulp-sass')(require('sass'));
 import cssnano from "gulp-cssnano";
+import gulpif from "gulp-if";
 //const clean = require('gulp-clean');
 
 const PRODUCTION = yargs.argv.prod;
@@ -10,7 +11,7 @@ export const styles = () => {
     return gulp.src('src/assets/scss/bundle.scss')
         .pipe(sass().on('ScssError', sass.logError))
         //.pipe(clean())
-        .pipe(cssnano())
+        .pipe(gulpif(PRODUCTION, cssnano()))
         .pipe(gulp.dest('dist/assets/css'));
 }
 
