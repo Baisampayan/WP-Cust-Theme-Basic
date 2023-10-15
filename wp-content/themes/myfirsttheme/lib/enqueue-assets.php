c741a4755bcd8e320b749fdd0381c2d08ff53e82
Baisampayan <baisampayandas@gmail.com>
2023-10-15 22:40:22 +0530

    General System fixes

--- wp-content/themes/myfirsttheme/lib/enqueue-assets.php
+++ wp-content/themes/myfirsttheme/lib/enqueue-assets.php
@@ -1,7 +1,7 @@
 <?php
 
 function myfirsttheme_assets() {
-    wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . './dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
+    wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all' );
 }
 
 add_action( 'wp_enqueue_style', 'myfirsttheme_assets' );
