21370fd7650a3aa7a0a93708bff2ede117c3f0bf
Baisampayan <baisampayandas@gmail.com>
2023-10-15 23:13:13 +0530

    Installing Gulp-SourceMap

--- wp-content/themes/myfirsttheme/gulpfile.babel.js
+++ wp-content/themes/myfirsttheme/gulpfile.babel.js
@@ -3,16 +3,19 @@ import yargs from "yargs";
 const sass = require('gulp-sass')(require('sass'));
 import cssnano from "gulp-cssnano";
 import gulpif from "gulp-if";
+import sourcemaps from 'gulp-sourcemaps';
 //const clean = require('gulp-clean');
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
     return gulp.src('src/assets/scss/bundle.scss')
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass().on('ScssError', sass.logError))
         //.pipe(clean())
         // Only minify in  production (gulp styles -- prod)
         .pipe(gulpif(PRODUCTION, cssnano()))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
         .pipe(gulp.dest('dist/assets/css'));
 }
 
