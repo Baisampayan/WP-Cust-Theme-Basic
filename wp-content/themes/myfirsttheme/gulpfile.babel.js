0dfbf361f49467e2e92b6eb052f59d7c06c76cd3
Baisampayan <baisampayandas@gmail.com>
2023-10-16 20:24:58 +0530

    Installing Gulp-Imagemin: sudo npm install gulp-imagemin@7.1.0 --save-dev

--- wp-content/themes/myfirsttheme/gulpfile.babel.js
+++ wp-content/themes/myfirsttheme/gulpfile.babel.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 import cssnano from "gulp-cssnano";
 import gulpif from "gulp-if";
 import sourcemaps from 'gulp-sourcemaps';
-//const clean = require('gulp-clean');
+import imagemin from 'gulp-imagemin';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -12,20 +12,31 @@ const FolderPaths = {
     styles: {
         src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
         dest: 'dist/assets/css'
+    }, 
+    images: {
+        src: 'src/assets/images/**/*.{jpg,png,gif,webp,tiff,jpeg,svg}',
+        dest: 'dist/assets/images'
     }
 }
-
+// Style Optimization
 export const styles = () => {
     return gulp.src(FolderPaths.styles.src)
         .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass().on('ScssError', sass.logError))
-        //.pipe(clean())
         // Only minify in  production (gulp styles -- prod)
         .pipe(gulpif(PRODUCTION, cssnano()))
         .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
         .pipe(gulp.dest(FolderPaths.styles.dest));
 }
 
+// Image Optimization
+export const themeimagesmin = () => {
+    return gulp.src(FolderPaths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(FolderPaths.images.dest));
+}
+
+// Watch Task
 export const watch = () => {
     gulp.watch('src/assets/scss/**/*.scss', styles);
 }
