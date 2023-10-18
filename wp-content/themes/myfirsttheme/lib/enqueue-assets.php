19b3ea14fa865f001e922ec05aa697051d7aa9ec
Baisampayan <baisampayandas@gmail.com>
2023-10-19 02:09:19 +0530

    Adding JS file to the website frontend

--- wp-content/themes/myfirsttheme/lib/enqueue-assets.php
+++ wp-content/themes/myfirsttheme/lib/enqueue-assets.php
@@ -2,6 +2,8 @@
 
 function myfirsttheme_assets() {
     wp_enqueue_style( 'myfirsttheme-stylesheet', get_template_directory_uri( ) . '/dist/assets/css/bundle.css', array(), '1.0.1', 'all' );
+
+    wp_enqueue_script( 'myfirsttheme-script', get_template_directory_uri( ) . '/dist/assets/js/bundle.js', array(), true );
 }
 
 add_action( 'wp_enqueue_scripts', 'myfirsttheme_assets' );
