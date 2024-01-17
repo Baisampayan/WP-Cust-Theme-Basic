439ee50d8f4b22f1ef3fce20cd782d75893b82ef
Baisampayan <baisampayandas@gmail.com>
2024-01-17 11:54:35 +0530

    Updating all files to server

--- wp-content/themes/myfirsttheme/index.php
+++ wp-content/themes/myfirsttheme/index.php
@@ -15,7 +15,7 @@
                 <?php /* the_content( ); */?>
             <?php }
         } the_posts_pagination( );
-        // Creating Custom Action Hooks
+        /* Creating Custom Action Hooks */
         do_action( '_themename_after_pagination' );
     } else { ?>
         <p><?php esc_html_e( 'There is no post available according to your criteria.', '_themename' ); ?></p>
--- wp-content/themes/myfirsttheme/packaged/myfirsttheme/functions.php
+++ wp-content/themes/myfirsttheme/packaged/myfirsttheme/functions.php
@@ -3,4 +3,10 @@
 require_once('lib/helpers.php');
 require_once('lib/enqueue-assets.php');
 
+// Creating Custom Action Hooks
+function after_pagination() {
+    echo "Subscribe For Latest Updates";
+}
+add_action( 'myfirsttheme_after_pagination', 'after_pagination' );
+
 ?>
